56f15154e3af7c837f7c91d4d0982ddd11405668
wingcarl <wingcarlos5@gmail.com>
2014-11-26 10:41:23 +0800

    3

---
+++ day01/src/com/carl/xml/sax/SAXDemo2.java
@@ -0,0 +1,14 @@
+package com.carl.xml.sax;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+public class SAXDemo2 {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException {
+		SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
+		
+	}
+}
